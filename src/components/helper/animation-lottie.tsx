1595f5ecb508aa80dfa8eb993d2d5966d5c18e71
Nitesh <niteshshukla341@gmail.com>
2025-03-07 23:56:58 +0530

    add react-lottie

--- src/components/helper/animation-lottie.tsx
+++ src/components/helper/animation-lottie.tsx
@@ -1,14 +1,21 @@
-"use client"
+"use client";
+
+import { useEffect, useState } from "react";
 
 type GlowCardProps = {
   animationPath: any;
 };
-const AnimationLottie = ({ animationPath }:GlowCardProps) => {
-  console.log(animationPath)
 
-  return (
-    <div>re</div>
-  );
+const AnimationLottie = ({ animationPath }: GlowCardProps) => {
+  const [Lottie, setLottie] = useState<any>(null);
+
+  useEffect(() => {
+    import("lottie-react").then((mod) => setLottie(() => mod.default));
+  }, []);
+
+  if (!Lottie) return null;
+
+  return <Lottie animationData={animationPath} loop autoplay style={{ width: "95%" }} />;
 };
 
 export default AnimationLottie;
