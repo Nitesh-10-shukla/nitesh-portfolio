c96559196d0ba6c7a962417c4c16d162359c722c
Nitesh <niteshshukla341@gmail.com>
2025-03-07 23:24:36 +0530

    remove client provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-
+import AboutSection from "@/components/homepage/about/AboutSection";
+import ContactSection from "@/components/homepage/contact/ContactSection";
+import Education from "@/components/homepage/education/Education";
+import Experience from "@/components/homepage/experience/Experience";
+import HeroSection from "@/components/homepage/hero-section/HeroSection";
+import Projects from "@/components/homepage/projects/Projects";
+import Resume from "@/components/homepage/resume/Resume";
+import Skills from "@/components/homepage/skills/Skills";
 
 export default function Home() {
   return (
     <div>
-    test
+      <HeroSection />
+      <AboutSection />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Resume />
+      <Education />
+      <ContactSection />
     </div>
   );
 }
