8674e6fe724b9c1f41adea7bc966ebc64024034e
Nitesh <niteshshukla341@gmail.com>
2025-03-07 23:16:51 +0530

    remove unused var

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -2,7 +2,6 @@
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import dynamic from "next/dynamic";
 import ScrollToTop from "./helper/scroll-to-top";
 
 export default function ClientProviders() {
